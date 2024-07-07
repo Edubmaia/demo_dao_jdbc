ff216bd1d579682a5a429561cf6e180f053d8928
Eduardo Maia <eduardobmaia@hotmail.com>
2024-07-07 10:36:46 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -15,7 +16,7 @@ public class Program {
 
 		System.out.println("=== Test 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-
+		
 		System.out.println(seller);
 
 		System.out.println("\n=== Test 2: seller findByDepartment ====");
@@ -30,6 +31,10 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		System.out.println("\n=== Test 4: seller insert ====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(),4000.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 	}
 }
